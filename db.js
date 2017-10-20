b195cca9d4f85103c05491e3839027cdcc358f74
Ashish Ingle <ingle.ashish@gmail.com>
2017-10-21 02:35:15 +0530

    added configuration for test db if app run in test mode

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 //use node native promise
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/todoapp", { useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI , { useMongoClient: true});
 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+console.log("Env: ", ENV);
+
+if(ENV === 'development'){
+  process.env.PORT = 8080
+  process.env.MONGODB_URI = "mongodb://localhost:27017/todoapp"
+} else if(ENV === 'test'){
+  process.env.PORT = 8080
+  process.env.MONGODB_URI = "mongodb://localhost:27017/test_todoapp"
+}
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const db = require("./db");
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 const app = express();
 
